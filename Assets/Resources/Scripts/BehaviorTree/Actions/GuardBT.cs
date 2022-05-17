107431bc62043eb7f29e4cf1258be362b7ff87db
anthonyspano <adspano91@gmail.com>
2022-05-17 09:17:21 -0600

    BT

---
+++ Assets/Resources/Scripts/BehaviorTree/Actions/GuardBT.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BehaviorTree;
+public class GuardBT : BehaviorTree.Tree
+{
+    public UnityEngine.Transform[] waypoints;
+    public static float speed = 2f;
+
+    protected override Node SetupTree()
+    {
+        Node root = new TaskPatrol(transform, waypoints);
+        return root;
+    }
+
+}
---
+++ Assets/Resources/Scripts/BehaviorTree/Actions/TaskPatrol.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BehaviorTree;
+
+public class TaskPatrol : Node
+{
+    private Transform _transform;
+    private Animator _animator;
+    private Transform[] _waypoints;
+
+    private int _currentWaypointIndex = 0;
+
+    private float _waitTime = 2.2f;
+    private float _waitCounter = 0f;
+    private bool _waiting = false;
+    private float _speed;
+
+
+    public TaskPatrol(Transform transform, Transform[] waypoints) 
+    {
+        _transform = transform;
+        _animator = transform.GetComponent<Animator>();
+        _waypoints = waypoints;
+    }
+
+    public override NodeState Evaluate()
+    {
+        if(_waiting)
+        {
+            _waitCounter += Time.deltaTime;
+            if(_waitCounter >= _waitTime)
+            {
+                _waiting = false;
+                _animator.SetBool("Walking", true);
+            }
+        }
+        else
+        {
+            Transform wp = _waypoints[_currentWaypointIndex];
+            if(Vector2.Distance(_transform.position, wp.position) < 0.1f)
+            {
+                _animator.SetBool("Walking", false);
+                _transform.position = wp.XandY();
+                _waitCounter = 0f;
+                _waiting = true;
+
+                _currentWaypointIndex = (_currentWaypointIndex + 1) % _waypoints.Length;
+                Debug.Log(_currentWaypointIndex);
+            }
+            else
+            {
+                _transform.position = Vector2.MoveTowards(_transform.position, wp.position, 8f * Time.deltaTime);
+            }
+
+        }
+
+        state = NodeState.RUNNING;
+        return state;
+    }
+}
