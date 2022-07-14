c31e07deafd0b7b5b4ec598e43d37bb11104b17a
anthonyspano <adspano91@gmail.com>
2022-07-14 14:44:55 -0600

    ip

--- Assets/Resources/Scripts/BehaviorTree/Actions/TaskGoToTargetAndAttack.cs
+++ Assets/Resources/Scripts/BehaviorTree/Actions/TaskGoToTargetAndAttack.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using BehaviorTree;
 using UnityEngine;
+using DebugTools;
+
+
 
 // go to the target's tracked position and hit the ground
 public class TaskGoToTargetAndAttack : Node
@@ -10,6 +13,10 @@ public class TaskGoToTargetAndAttack : Node
     private Animator _animator;
 
     private EnemyManager _enemyManager;
+    
+    // attack counter
+    private float _attackTime = MinotaurAttr.atkSpd;
+    private float _attackCounter = MinotaurAttr.atkSpd+1;
 
     public TaskGoToTargetAndAttack(Transform transform)
     {
@@ -20,9 +27,13 @@ public class TaskGoToTargetAndAttack : Node
 
     public override NodeState Evaluate()
     {
+        WindowStats.Value = _attackCounter;
+        
         Transform target = (Transform)GetData("target");
 
         EnemyManager.Flipped = (target.position.x > _transform.position.x) ? true : false;
+
+        _attackCounter += Time.deltaTime;
         
         if (Vector3.Distance(_transform.position, target.position) > 10f && _enemyManager.CanMove)
         {
@@ -36,8 +47,24 @@ public class TaskGoToTargetAndAttack : Node
             _enemyManager.CanMove = false;
             
             // attack()
-            if(!_animator.GetCurrentAnimatorStateInfo(0).IsName("mino_atk1"))
-                _animator.Play("Attack");
+            if (_attackCounter >= _attackTime)
+            {
+                if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("mino_atk1"))
+                    _animator.Play("Attack");
+                
+                // check if player is dead
+                if (PlayerManager.Instance.pHealth.GetHealth() <= 0)
+                {
+                    ClearData("target");
+                    _animator.Play("Patrol");
+                }
+                else
+                {
+                    _attackCounter = 0f;
+                }
+
+
+            }
         }
 
         state = NodeState.RUNNING;
