c211685569965ab4142575e9ccf0cf7cc4d0bdd1
anthonyspano <adspano91@gmail.com>
2023-04-26 12:52:12 -0600

    interaction prompt

--- Assets/Utility/Interactable.cs
+++ Assets/Utility/Interactable.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     public AudioSource audioSource;
+    public Object icon;
+    private Object prompt;
+    bool once;
 
     void Start()
     {
@@ -13,9 +16,20 @@ public class Interactable : MonoBehaviour
 
     void OnCollisionStay2D(Collision2D col)
     {
+        // prompt comes up
+        // generate image
+        if(!once)
+        {
+            once = true;
+            prompt = GameObject.Instantiate(icon, transform.position + new Vector3(1,1,50), Quaternion.identity);
+        }
 
         if(col.gameObject.CompareTag("Player") && PlayerInput.Interact())
         {
+            // remove prompt
+            once = false;
+            Object.Destroy(prompt);
+
             audioSource.Play();
 
             var doorExit = transform.GetChild(0);
