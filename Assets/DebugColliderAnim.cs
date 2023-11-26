510338197c1bc6b445039d28d7150ace0123dbee
anthonyspano <adspano91@gmail.com>
2023-11-26 10:16:34 -0700

    fixing collider in progress

---
+++ Assets/DebugColliderAnim.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugColliderAnim : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            anim.Play("DebugAttack");
+        }
+    }
+}
